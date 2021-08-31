83521fcef80276b27bee9e974b0ccff99b18c060
emon1995 <emonhossain995@gmail.com>
2021-08-31 13:02:51 +0600

    Create Next App Layout

---
+++ components/Layout.js
@@ -0,0 +1,30 @@
+import { AppBar, Container, Toolbar, Typography } from '@material-ui/core'
+import Head from 'next/head'
+import React from 'react'
+
+const Layout = ({children}) => {
+    return (
+        <div>
+            <Head>
+                <title>Next Amazona</title>
+            </Head>
+            <AppBar position='static'>
+                <Toolbar>
+                    <Typography>
+                        amazona
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+            <Container>
+                {children}
+            </Container>
+            <footer>
+                <Typography>
+                    All rights reserved. Next Amazona
+                </Typography>
+            </footer>
+        </div>
+    )
+}
+
+export default Layout

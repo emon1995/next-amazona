98ea65e7fddf8763a38cae175db58f453c9f0808
emon1995 <emonhossain995@gmail.com>
2021-08-31 21:47:21 +0600

    route Product Link

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import { Button, Card, CardActionArea, CardActions, CardContent, CardMedia, Grid, Typography } from '@material-ui/core';
+import NextLink from "next/link";
 import Layout from '../components/Layout';
 import data from '../utils/data';
 
@@ -12,6 +13,7 @@ export default function Home() {
           {data.products.map((product) => (
             <Grid item md={4} key={product.name}>
               <Card>
+                <NextLink href={`/product/${product.slug}`} passHref>
                 <CardActionArea>
                   <CardMedia
                     component="img"
@@ -22,6 +24,7 @@ export default function Home() {
                     <Typography>{product.name}</Typography>
                   </CardContent>
                 </CardActionArea>
+                </NextLink>
                 <CardActions>
                   <Typography>${product.price}</Typography>
                   <Button size="small" color="primary">

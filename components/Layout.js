4514c6af0bf647e38fd09972e739438331ee18cc
emon1995 <emonhossain995@gmail.com>
2021-08-31 21:18:22 +0600

    Add Header Link

--- components/Layout.js
+++ components/Layout.js
@@ -1,7 +1,8 @@
-import { AppBar, Container, Toolbar, Typography } from '@material-ui/core'
+import { AppBar, Container, Link, Toolbar, Typography } from '@material-ui/core'
 import Head from 'next/head'
 import React from 'react'
 import useStyles from '../utils/styles'
+import NextLink from 'next/link'
 
 const Layout = ({children}) => {
     const classes = useStyles();
@@ -13,9 +14,22 @@ const Layout = ({children}) => {
             </Head>
             <AppBar position='static' className={classes.navbar}>
                 <Toolbar>
-                    <Typography>
+                    <NextLink href='/' passHref>
+                        <Link>
+                    <Typography className={classes.brand}>
                         amazona
                     </Typography>
+                    </Link>
+                    </NextLink>
+                    <div className={classes.grow}></div>
+                    <div>
+                        <NextLink href='/cart' passHref>
+                            <Link>Cart</Link>
+                        </NextLink>
+                        <NextLink href='/login' passHref>
+                            <Link>Login</Link>
+                        </NextLink>
+                    </div>
                 </Toolbar>
             </AppBar >
             <Container className={classes.main}>

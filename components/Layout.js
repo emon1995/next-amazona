4e094c8939d6a0d9d9049d11520f1199866a566d
emon1995 <emonhossain995@gmail.com>
2021-09-01 12:07:40 +0600

    Add MaterialUI Theme

--- components/Layout.js
+++ components/Layout.js
@@ -1,48 +1,75 @@
-import { AppBar, Container, Link, Toolbar, Typography } from '@material-ui/core'
-import Head from 'next/head'
-import React from 'react'
-import useStyles from '../utils/styles'
-import NextLink from 'next/link'
+import {
+    AppBar,
+    Container,
+    createTheme, CssBaseline, Link,
+    ThemeProvider,
+    Toolbar,
+    Typography
+} from '@material-ui/core';
+import Head from 'next/head';
+import NextLink from 'next/link';
+import React from 'react';
+import useStyles from '../utils/styles';
 
-const Layout = ({title, description, children}) => {
-    const classes = useStyles();
+const Layout = ({ title, description, children }) => {
+  const theme = createTheme({
+    typography: {
+      h1: {
+        fontSize: '1.6rem',
+        fontWeight: 400,
+        margin: '1rem 0',
+      },
+      h2: {
+        fontSize: '1.4rem',
+        fontWeight: 400,
+        margin: '1rem 0',
+      },
+    },
+    palette:{
+        type: 'light',
+        primary: {
+            main: '#f0c000',
+        },
+        secondary: {
+            main: '#208080',
+        }
+    }
+  });
+  const classes = useStyles();
 
-    return (
-        <div>
-            <Head>
-                <title>{title ? `${title} - Next Amazona` : 'Next Amazona'}</title>
-                {description && <meta name='description' content={description}></meta>}
-            </Head>
-            <AppBar position='static' className={classes.navbar}>
-                <Toolbar>
-                    <NextLink href='/' passHref>
-                        <Link>
-                    <Typography className={classes.brand}>
-                        amazona
-                    </Typography>
-                    </Link>
-                    </NextLink>
-                    <div className={classes.grow}></div>
-                    <div>
-                        <NextLink href='/cart' passHref>
-                            <Link>Cart</Link>
-                        </NextLink>
-                        <NextLink href='/login' passHref>
-                            <Link>Login</Link>
-                        </NextLink>
-                    </div>
-                </Toolbar>
-            </AppBar >
-            <Container className={classes.main}>
-                {children}
-            </Container>
-            <footer className={classes.footer}>
-                <Typography>
-                    All rights reserved. Next Amazona
-                </Typography>
-            </footer>
-        </div>
-    )
-}
+  return (
+    <div>
+      <Head>
+        <title>{title ? `${title} - Next Amazona` : 'Next Amazona'}</title>
+        {description && <meta name='description' content={description}></meta>}
+      </Head>
+      <ThemeProvider theme={theme}>
+          <CssBaseline />
+        <AppBar position='static' className={classes.navbar}>
+          <Toolbar>
+            <NextLink href='/' passHref>
+              <Link>
+                <Typography className={classes.brand}>amazona</Typography>
+              </Link>
+            </NextLink>
+            <div className={classes.grow}></div>
+            <div>
+              <NextLink href='/cart' passHref>
+                <Link>Cart</Link>
+              </NextLink>
+              <NextLink href='/login' passHref>
+                <Link>Login</Link>
+              </NextLink>
+            </div>
+          </Toolbar>
+        </AppBar>
+        <Container className={classes.main}>{children}</Container>
+        <footer className={classes.footer}>
+          <Typography>All rights reserved. Next Amazona</Typography>
+        </footer>
+      </ThemeProvider>
+    </div>
+  );
+};
 
-export default Layout
+export default Layout;
